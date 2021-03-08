6f2fce6956d7e8e1ded6534c7195dfc963d18dcc
rabeeh-ta <rabeehchemnad@gmail.com>
2021-03-08 19:04:11 +0530

    front end routes seting

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 require('./db/mongoose');
+const path = require('path');
 const express = require('express');
 
 const codeRouter = require('./routes/codeRoutes');
+const frontRouter = require('./routes/frontEnd');
 
 //? setup express
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 //? express configurations
 app.use(express.json());
+app.use(express.static('public'));
 app.use(codeRouter);
+app.use(frontRouter);
 
 app.listen(port, () => {
   console.log('Express running on port ' + port);
