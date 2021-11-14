277892ca0ffd54a57d32e5b8bb3909df1b6ec8f4
rabeeh-ta <rabeehchemnad@gmail.com>
2021-11-14 16:43:27 +0530

    check all args are passed && user allready exist

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,8 +5,22 @@ const router = new express.Router();
 
 //? register a new user.
 router.post('/register', async (req, res) => {
-  const user = new User(req.body);
-  console.log(req.body);
+  // make sure everything is passed ie, name,email,password
+  var { name, email, password } = req.body;
+  email = email.toLowerCase();
+  if (!(name && email && password)) {
+    return res.status(400).send('pass in name, email, password as JSON object');
+  }
+
+  // check if user already exists
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    return res.status(409).send('User already exist, Please Login');
+  }
+
+  // save new user to DB
+  const user = new User({ user: name, email, password });
+
   try {
     await user.save();
     res.status(201).send('user ' + user.user + ' saved.');
@@ -14,6 +28,7 @@ router.post('/register', async (req, res) => {
   } catch (e) {
     res.status(400).send(e);
   }
+  // res.status(200).send('success');
 });
 
 module.exports = router;
