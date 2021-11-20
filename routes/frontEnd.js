625e417d228aaf1761b2f788748d1e4712446eb9
rabeeh-ta <rabeehchemnad@gmail.com>
2021-11-20 14:33:16 +0530

    if no JWT when requesting /admin then return to /login

--- routes/frontEnd.js
+++ routes/frontEnd.js
@@ -13,7 +13,7 @@ var authenticateJWT = function (req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1]; //if there is a jwt token passed then return the token else UNDEFINED, the split(' ')[1] will get the token part from the string passed in the Header "Bearer KEY"
 
-  if (token == null) return res.sendStatus(401); // if no token is passed then no access granted login
+  if (token == null) return res.redirect('/login'); // if no token is passed then no access granted login
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403); // you have a token but it is not valid
@@ -35,8 +35,12 @@ router.get('/post', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/src/post.html'));
 });
 
+router.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/src/login.html'));
+});
+
 router.get('/admin', authenticateJWT, (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/src/admin.html'));
+  res.sendFile(path.join(__dirname, '../guardedPages/admin.html'));
 });
 
 router.get('/*', (req, res) => {
