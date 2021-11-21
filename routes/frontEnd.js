69b0e204ccb7f9884aea7c12e5dccfd177f12fd2
rabeeh-ta <rabeehchemnad@gmail.com>
2021-11-21 10:25:46 +0530

    auth middleware works with cookie based authentication

--- routes/frontEnd.js
+++ routes/frontEnd.js
@@ -9,9 +9,13 @@ const router = new express.Router();
 
 //? middle ware function where it will check if the JWT that the user has send is valid or not.
 var authenticateJWT = function (req, res, next) {
-  console.log('middleware is on baby');
+  //! "Bearer JWT" authorization from req header.
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; //if there is a jwt token passed then return the token else UNDEFINED, the split(' ')[1] will get the token part from the string passed in the Header "Bearer KEY"
+  var token = authHeader && authHeader.split(' ')[1]; //if there is a jwt token passed then return the token else UNDEFINED, the split(' ')[1] will get the token part from the string passed in the Header "Bearer KEY"
+
+  //! "cookie" based authorization from req header.
+  //? if there is jwt in req cookie then that is the token
+  if (req.cookies.authorization != undefined) token = req.cookies.authorization;
 
   if (token == null) return res.redirect('/login'); // if no token is passed then no access granted login
 
