8b3171b9d2bf5415a880b15c5c0e1556e9943178
rabeeh-ta <rabeehchemnad@gmail.com>
2022-01-16 12:12:24 +0530

    refactor: jwt authentication to new file

--- routes/frontEnd.js
+++ routes/frontEnd.js
@@ -1,30 +1,10 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
+const { authenticateJWT } = require('../middleware/authJWT');
 const path = require('path');
 const express = require('express');
 
 const { dirname } = require('path');
 
 const router = new express.Router();
-
-//? middle ware function where it will check if the JWT that the user has send is valid or not.
-var authenticateJWT = function (req, res, next) {
-  //! "Bearer JWT" authorization from req header.
-  const authHeader = req.headers['authorization'];
-  var token = authHeader && authHeader.split(' ')[1]; //if there is a jwt token passed then return the token else UNDEFINED, the split(' ')[1] will get the token part from the string passed in the Header "Bearer KEY"
-
-  //! "cookie" based authorization from req header.
-  //? if there is jwt in req cookie then that is the token
-  if (req.cookies.authorization != undefined) token = req.cookies.authorization;
-
-  if (token == null) return res.redirect('/login'); // if no token is passed then no access granted login
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403); // you have a token but it is not valid
-    req.user = user; // user => the decrypted value which was inside the JWT
-    next();
-  });
-};
 
 //? frontend html serving routes
 router.get('/', (req, res) => {
