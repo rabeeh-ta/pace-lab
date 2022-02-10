c36a53c69955164a0caf7e128aced2e5fd622f54
rabeeh-ta <rabeehchemnad@gmail.com>
2022-02-10 18:24:28 +0530

    prod db link change

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    'mongodb+srv://rabeeh:rabeeh9895@prod-cluster.ctha1.mongodb.net/devMode?retryWrites=true&w=majority',
+    'mongodb+srv://rabeeh:rabeeh9895@prod-cluster.ctha1.mongodb.net/codeShare?retryWrites=true&w=majority',
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
   )
   .then(() => {
