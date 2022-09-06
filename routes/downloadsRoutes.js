aa5d48978f385961ed8210972d8b0e2b9fe918e1
rabeeh-ta <rabeehchemnad@gmail.com>
2022-09-06 16:05:30 +0530

    list directories in the download folder

--- routes/downloadsRoutes.js
+++ routes/downloadsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const router = new express.Router();
 
 router.get('/download/:sub/:programNo', (req, res) => {
-  var currentPath = path.join(
+  const currentPath = path.join(
     __dirname,
     '..',
     'downloads',
@@ -13,6 +14,28 @@ router.get('/download/:sub/:programNo', (req, res) => {
   );
 
   res.sendFile(`${currentPath}`);
+});
+
+router.get('/download/ls', (req, res) => {
+  let dirs = [];
+  let files = [];
+  const currentPath = path.join(__dirname, '..', 'downloads');
+
+  // search directories
+  fs.readdirSync(currentPath).forEach((dir) => {
+    if (!(dir[0] == '.')) dirs.push(dir);
+  });
+
+  //search files
+  dirs.forEach((dir, index) => {
+    let subFiles = [];
+    fs.readdirSync(currentPath + '/' + dir).forEach((file) => {
+      if (!(file[0] == '.')) subFiles.push(file);
+    });
+    files[index] = subFiles;
+  });
+
+  res.send(`${dirs} \n ${files}`);
 });
 
 module.exports = router;
