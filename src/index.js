a2039395fcc8ab6ae68e31bca9910ab1f25cc628
Rabeeh T A <rabeehchemnad@gmail.com>
2022-12-08 21:57:23 +0530

    reset project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-import Sidebar from './Sidebar';
-import SearchBar from './SearchBar';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className="">
-      <Sidebar />
-      <SearchBar />
-    </div>
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
